a6f56d80081bd5d05c05817240885322100def65
Sergej Kukharev <kukharev@narod.ru>
2024-08-06 09:21:53 +0300

    Спринт 14/23: 14 → Тема 3/4: CollectionViewLayout → Урок 4/4

---
+++ Tracker/Models/Tracker.swift
@@ -0,0 +1,8 @@
+//
+//  Tracker.swift
+//  Tracker
+//
+//  Created by Сергей Кухарев on 31.07.2024.
+//
+
+import Foundation
---
+++ Tracker/Models/TrackerCategory.swift
@@ -0,0 +1,8 @@
+//
+//  TrackerCategory.swift
+//  Tracker
+//
+//  Created by Сергей Кухарев on 31.07.2024.
+//
+
+import Foundation
