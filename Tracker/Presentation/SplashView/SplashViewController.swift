d5049430746f6c24d0a7b7b508ad9908cd966e3a
Sergej Kukharev <kukharev@narod.ru>
2024-08-30 14:40:32 +0300

    SplashView: оптимизация перечислений с константами класса

--- Tracker/Presentation/SplashView/SplashViewController.swift
+++ Tracker/Presentation/SplashView/SplashViewController.swift
@@ -11,13 +11,10 @@ import UIKit
 final class SplashViewController: UIViewController {
     // MARK: - Types
 
-    private enum Identifiers {
+    private enum Constants {
         static let applicationLogoImageName = "AppLogo"
         static let trackersTabBarImageName = "TrackersTabBarImage"
         static let statisticsTabBarImageName = "StatisticsTabBarImage"
-    }
-
-    private enum Constants {
         static let trackersTabBarItemTitle = "Трекеры"
         static let statisticsTabBarItemTitle = "Статистика"
     }
@@ -28,7 +25,7 @@ final class SplashViewController: UIViewController {
     private lazy var applicationLogo: UIImageView = {
         let image = UIImageView()
         image.translatesAutoresizingMaskIntoConstraints = false
-        guard let appImage = UIImage(named: Identifiers.applicationLogoImageName) else {
+        guard let appImage = UIImage(named: Constants.applicationLogoImageName) else {
             assertionFailure("Ошибка загрузки логотипа приложения")
             return image
         }
@@ -67,12 +64,12 @@ final class SplashViewController: UIViewController {
     private func switchToMainScreen() {
         // Создание вью контроллера для экрана с трекерами
         let trackersViewController = TrackersViewController()
-        trackersViewController.tabBarItem = UITabBarItem(title: Constants.trackersTabBarItemTitle, image: UIImage(named: Identifiers.trackersTabBarImageName), tag: 1)
+        trackersViewController.tabBarItem = UITabBarItem(title: Constants.trackersTabBarItemTitle, image: UIImage(named: Constants.trackersTabBarImageName), tag: 1)
         let trackersViewPresenter = TrackersViewPresenter()
         trackersViewController.configure(trackersViewPresenter)
         // Создание вью контроллера для экрана со статистикой
         let statisticsViewController = StatisticsViewController()
-        statisticsViewController.tabBarItem = UITabBarItem(title: Constants.statisticsTabBarItemTitle, image: UIImage(named: Identifiers.statisticsTabBarImageName), tag: 2)
+        statisticsViewController.tabBarItem = UITabBarItem(title: Constants.statisticsTabBarItemTitle, image: UIImage(named: Constants.statisticsTabBarImageName), tag: 2)
         let statisticsViewPresenter = StatisticsViewPresenter()
         statisticsViewController.configure(statisticsViewPresenter)
         // Создание вью контроллера для таб-бара
@@ -90,7 +87,7 @@ final class SplashViewController: UIViewController {
         tabBarController.tabBar.addTopBorder(with: .appTabBarTopBorder, andWidth: 1)
         // Получаем экземпляр `window` приложения
         guard let window = UIApplication.shared.windows.first else {
-            assertionFailure("Invalid window configuration")
+            assertionFailure("Ошибка получения ссылки на экран устройства")
             return
         }
         window.rootViewController = tabBarController
