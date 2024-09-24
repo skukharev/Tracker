9b6f324b6784d79ad33676e80c81efe49f160bb6
Sergej Kukharev <kukharev@narod.ru>
2024-09-24 19:21:40 +0300

    Приведение градиента на экране статистики в соответствие с дизайном

--- Tracker/Presentation/StatisticsView/View/StatisticsCollectionViewCell.swift
+++ Tracker/Presentation/StatisticsView/View/StatisticsCollectionViewCell.swift
@@ -97,8 +97,8 @@ final class StatisticsCollectionViewCell: UICollectionViewCell {
         self.layer.masksToBounds = true
 
         gradientLayer.colors = [UIColor.red.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
         gradientLayer.frame = self.bounds
         gradientLayer.cornerRadius = self.layer.cornerRadius
         self.layer.insertSublayer(gradientLayer, at: 0)
